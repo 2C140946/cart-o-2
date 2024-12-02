18149157e8388a15818588da0be51b88bda1ff6b
2C140946 <00001124769705sp@al.educacao.sp.gov.br>
2024-12-02 09:06:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes da disney',
+    'Quantos filmes a Disney já criou?',
+    '59 animações em longa-metragem feitas pela Walt Disney Animation Studios'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Egito?',
+    'A capital do Egito é Cairo'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é Bye (BAI)'
+)
